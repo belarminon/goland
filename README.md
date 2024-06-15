e32ffece47584fa785c94c25956b5891cffcfdb8
belarminon <35656300+belarminon@users.noreply.github.com>
2024-06-15 17:34:59 +0000

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # goland
+
+para compilar faz-se: go build main.go enter
+
+paracompilar pro windows faz-se: GOOS=windows go build main.go
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+	"time"
+)
+
 func main()  {
-	println("Hello, World!")
+	// http.HandleFunc("/", HelloWorld)
+	// http.ListenAndServe(":8888", nil)
+	go contador(10)
+	go contador(10)
+	contador(10)
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Hello, World!"))
+}
+
+func contador(qtd int)  {
+		for i := range qtd{
+			fmt.Println(i)
+			time.Sleep(time.Second)
+		};
 }
